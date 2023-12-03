a5d7ae2de02fd1d6731e13ed8a5ba33aab3fcc0b
gaunah <christoph@zelinski.de>
2023-12-03 21:32:36 +0100

    Solution for day03 part 2

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let input = include_str!("../input.txt");
     println!("Answer part1: {}", part1(input));
+    println!("Answer part2: {}", part2(input));
 }
 
 fn part1(input: &str) -> u32 {
@@ -11,11 +12,7 @@ fn part1(input: &str) -> u32 {
             left.chars()
                 .find_map(|c| {
                     if right.contains(c) {
-                        if c.is_lowercase() {
-                            Some(c as u32 - 96) // a-z -> 1-26
-                        } else {
-                            Some(c as u32 - 38) // A-Z -> 27-52
-                        }
+                        Some(calculate_item_prio(c))
                     } else {
                         None
                     }
@@ -25,13 +22,56 @@ fn part1(input: &str) -> u32 {
         .sum()
 }
 
-#[test]
-fn case1() {
-    let input = "vJrwpWtwJgWrhcsFMMfFFhFp
+fn part2(input: &str) -> u32 {
+    input
+        .lines()
+        .collect::<Vec<&str>>()
+        .chunks(3)
+        .map(|chunk| {
+            let [first, second, third] = chunk else {
+                panic!("The chunk has less then three elements!");
+            };
+
+            first
+                .chars()
+                .find_map(|c| {
+                    if second.contains(c) && third.contains(c) {
+                        Some(calculate_item_prio(c))
+                    } else {
+                        None
+                    }
+                })
+                .unwrap_or(0)
+        })
+        .sum()
+}
+
+fn calculate_item_prio(c: char) -> u32 {
+    if c.is_lowercase() {
+        c as u32 - 96 // a-z -> 1-26
+    } else {
+        c as u32 - 38 // A-Z -> 27-52
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw";
-    assert_eq!(part1(input), 157)
+
+    #[test]
+    fn case1() {
+        assert_eq!(part1(TEST_INPUT), 157);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(part2(TEST_INPUT), 70);
+    }
 }
